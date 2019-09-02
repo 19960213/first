d4db1a1963692b21c72142a976c7358233b457ad
lastrome96 <rmatjd2003@gmail.com>
2019-09-02 17:23:52 +0900

    2

--- Day01/src/Data03.java
+++ Day01/src/Data03.java
@@ -1,27 +1,9 @@
-/* 자바의 산술 (수학) 연산자
- * 1. +(덧셈), -(뺄셈), *(곱셈), /(나눗셈),
- * %(나머지)
+/* �옄諛붿쓽 �궛�닠 (�닔�븰) �뿰�궛�옄
+ * 1. +(�뜤�뀍), -(類꾩뀍), *(怨깆뀍), /(�굹�닓�뀍),
+ * %(�굹癒몄�)
  */
 public class Day03 {
 
-	public static void main(String[] args) {
-		int a=9;
-		int b=3;
 		
-		int re=0;
-		
-		re=a+b;
-		System.out.println(a+"+"+b+"="+re);
-		
-		re=a-b;
-		System.out.println(a+"-"+b+"="+re);
-		
-		re=a*b;
-		System.out.println(a+"*"+b+"="+re);
-		
-		re=a/b;
-		System.out.println(a+"/"+b+"="+re);
-		
-	}
 
 }
